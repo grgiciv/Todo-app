1a9824a4cd32ef5d1374e4a969e30e3cbb486731
grgiciv <118084227+grgiciv@users.noreply.github.com>
2023-03-31 19:39:11 +0200

    Create new items

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,32 @@ const defaultItems = [
 
 function App() {
   const [items, setItems] = useState(defaultItems);
+  const [formState, setFormState] = useState({
+    text:'',
+  });
+
+  const handleChange = (event) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setItems([
+      ...items,
+      {
+        id: Date.now(),
+        text: formState.text,
+        done: false,
+      }
+    ]);
+    setFormState({ ...formState, text: '' });
+  }
 
   const itemComponents = items.map(item => {
+    
     const handleChange = () => {
       console.log('handle change for item', item);
       setItems(items.map(newItem => {
@@ -27,9 +51,16 @@ function App() {
       }));
     };
 
+    const handleClick = () => {
+      setItems (items.filter(newItem => {
+        return newItem.id !== item.id;
+      }));
+    };
+
     return (
       <div key={item.id}>
         <input type="checkbox" checked={item.done} onChange={handleChange}/>{item.text}
+        <button onClick={handleClick}>X</button>
       </div>
     )
   })
@@ -37,6 +68,10 @@ function App() {
   return (
     <div>
       <h1>TODO APP</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="text" value={formState.text} onChange={handleChange}></input>
+        <button type='submit'>Add item</button>
+      </form>
       {itemComponents}
     </div>
   )
